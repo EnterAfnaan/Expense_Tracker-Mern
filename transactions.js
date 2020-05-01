2283daaa2bbecbd16f4f231110e3db21ffe550b6
EnterAfnaan <afnaankhan11@gmail.com>
2020-05-01 18:23:54 +0530

    Add files via upload

--- transactions.js
+++ transactions.js
@@ -1,87 +1,15 @@
-const Transaction = require('../models/Transaction');
+const express = require('express');
+const router = express.Router();
 
-//@desc GET all transactions
-//@route GET /api/v1/transactions
-//@access public
-exports.getTransactions = async (req ,res ,next) => {
-    try {
-        const transactions = await Transaction.find();
+const {getTransactions, addTransactions, deleteTransactions} = require('../controller/transactions');
 
-        return res.status(200).json({
-            success: true,
-            count: transactions.length,
-            data: transactions
-        });
-    } catch (err) {
-        return res.status(500).json({ 
-            success: false,
-            error: ' server error' 
-        });
-    }
-}  
+router
+    .route('/')
+    .get(getTransactions)
+    .post(addTransactions);
 
-//@desc Add transactions
-//@route POST /api/v1/transactions
-//@access public
-exports.addTransactions = async (req,res) => {
-    try {
-        const {text , amount} = req.body;
+router 
+    .route('/:id')  
+    .delete(deleteTransactions);    
 
-        const transaction = await Transaction.create(req.body);
-
-        return res.status(201).json({
-            success:true,
-            data: transaction 
-        });
-    } catch (err) {
-        if(err.name === 'ValidationError') {
-
-            const messages = Object.values(err.errors).map(val => val.message);
-
-            return res.status(400).json({
-                success: false,
-                error: messages
-            });
-        }
-        else {
-            return res.status(500).json({
-                success: false,
-                error : 'server error'
-            });
-            
-        }
-    }
-} 
-
-
-//@desc Delete transactions
-//@route DELETE /api/v1/transactions
-//@access public
-exports.deleteTransactions = async (req,res) => {
-    
-    try {
-        
-        const transaction = await Transaction.findById(req.params.id);
-
-    if(!transaction) {
-        return res.status(404).json({
-            success:false,
-            error: 'transaction not found'
-        });
-    }
-
-    await transaction.remove();
-
-    return res.status(200).json({
-        success:true,
-        data: {}
-    });
-
-    } catch (err) {
-       
-        return res.status(500).json({
-            success:false,
-            error: 'server Error'
-        });
-    }
-}  
+module.exports = router; 
